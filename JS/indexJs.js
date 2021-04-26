654a0f72bc3351e62d3c814c475009fc9d7b8459
JoshuaMesaGarcia <joshuamesgar@gmail.com>
2021-04-26 12:49:53 +0200

    Starting Branch Parallax

--- JS/indexJs.js
+++ JS/indexJs.js
@@ -32,4 +32,22 @@ function scrollToTop() {
   })
 }
 
-scrollToTopBtn.addEventListener("click", scrollToTop)
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+// Removing the Parallax effect in table size
+var screenTable = window.matchMedia("(max-width: 991.98px)");
+function removeClassTablet(screenTable) {
+  if (screenTable.matches) { // If media query matches
+    var landingPageDiv = document.getElementById("landingPage");
+    var projectDiv = document.getElementById("project");
+    var creativeDiv = document.getElementById("creative");
+
+     landingPageDiv.classList.remove("ParallaxContainer");
+     projectDiv.classList.remove("ContentContainer");
+     creativeDiv.classList.remove("ParallaxContainer2");
+
+  }
+}
+
+removeClassTablet(screenTable) // Call listener function at run time
+screenTable.addListener(removeClassTablet) // Attach listener function on state changes
