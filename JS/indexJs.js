06ed7c0ac60e1a3e64a59f081cd3f3f65fb19e20
JoshuaMesaGarcia <joshuamesgar@gmail.com>
2021-05-12 12:27:18 +0200

    Snapping Implemented

--- JS/indexJs.js
+++ JS/indexJs.js
@@ -22,32 +22,14 @@ $(document).ready(function() {
 });
 
 
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-var rootElement = document.documentElement;
-
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-
-scrollToTopBtn.addEventListener("click", scrollToTop);
-
-// Removing the Parallax effect in table size
-var screenTable = window.matchMedia("(max-width: 991.98px)");
-function removeClassTablet(screenTable) {
-  if (screenTable.matches) { // If media query matches
-    var landingPageDiv = document.getElementById("landingPage");
-    var projectDiv = document.getElementById("project");
-    var creativeDiv = document.getElementById("creative");
-
-     landingPageDiv.classList.remove("ParallaxContainer");
-     projectDiv.classList.remove("ContentContainer");
-     creativeDiv.classList.remove("ParallaxContainer2");
-
-  }
-}
-
-removeClassTablet(screenTable) // Call listener function at run time
-screenTable.addListener(removeClassTablet) // Attach listener function on state changes
+// var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+// var rootElement = document.documentElement;
+//
+// function scrollToTop() {
+//   rootElement.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   })
+//
+// }
+// scrollToTopBtn.addEventListener("click", scrollToTop);
